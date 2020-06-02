b92419295e0500a0c86566a0ebf2190c206d2fc2
Maria Souza <messenger.mariah2017@gmail.com>
2020-06-02 08:19:44 -0300

    resolução de exercicio

---
+++ revisao/questao.js
@@ -0,0 +1,87 @@
+// CLIENTES: 
+
+// 	CLIENTE1: 
+
+// 	Nome: Dora Barreto 
+// 	CPF: 223.001.221-29
+// 	Valor da compra: R$ 2.000,00
+// 	Modelo do celular: XXX-12
+// 	Ano do celular: 2020
+// 	Cor do celular: Amarelo
+// 	Loja: RJ-BR
+// 	Atendente: Joana Maria 
+
+
+// 	CLIENTE 2: 
+
+// 	Nome: Marcela Janino Silva 
+// 	CPF: 321.101.231-29
+// 	Valor da compra: R$ 1.300,00
+// 	Modelo do celular: XXM-y
+// 	Ano do celular: 2019
+// 	Cor do celular: ROXO
+// 	Loja: SP-BR
+// 	Atendente: Drielly Gomes Almeida
+
+
+// 	CLIENTE 3: 
+
+// 	Nome: Raquel Borges Figueredo
+// 	CPF: 910.311.425-01
+// 	Valor da compra: R$ 4.000,00
+// 	Modelo do celular: MDM-PRO
+// 	Ano do celular: 2020
+// 	Cor do celular: AZUL
+// 	Loja: MG-BR
+// 	Atendente: Fran Carneiro de Pinha
+
+
+
+
+const clientes = {
+    cliente1 = {
+
+        nome: 'Dora Barreto',
+        cpf: '223.001.221-29',
+        valor_da_compra: 2000,
+        modeloCelular: 'XXX-12',
+        anoCelular: 2020,
+        corCelular: 'Amarelo',
+        loja: 'Rj-Br',
+        atendente: 'Joana Maria '
+    },
+
+
+
+
+    cliente2 = {
+        nome: 'Marcela Janino Silva',
+        cpf: '321.101.231-29',
+        valor_da_compra: 1300,
+        modelo_do_celular: 'XXM-y',
+        ano_do_celular: 2019,
+        cor_do_celular: 'ROXO',
+        loja: 'Sp-Br',
+        atendente: ' Drielly Gomes Almeida '
+
+
+    },
+
+    cliente3 = {
+        nome: 'Raquel Borges Figueredo',
+        cpf: '910.311.425-01',
+        valor_da_compra: 4000,
+        modelo_do_celular: ' MDM-PRO',
+        ano_do_celular: 2020,
+        cor_do_celular: 'Azul',
+        loja: 'Mg-Br',
+        atendente: 'Fran Carneiro de Pinha'
+
+    }
+
+    
+};
+
+console.log('clientes', clientes);
+
+
